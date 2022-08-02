d05566f28b4a9762b5e44b72c96ce3b5c0e1fb94
Varden41 <arturpavlenko14@gmail.com>
2022-08-02 17:53:26 +0300

    task 2-3 complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player', {
+  id: '236203659',
+  width: 1080,
+});
+const VIDEOCUTTENT_TIME_KEY = 'videoplayer-current-time';
+const timer = e => {
+  localStorage.setItem(VIDEOCUTTENT_TIME_KEY, e.seconds);
+};
+
+player.on('timeupdate', throttle(timer, 1000));
+
+player
+  .setCurrentTime(localStorage.getItem(VIDEOCUTTENT_TIME_KEY))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+// Refs
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form input'),
+  message: document.querySelector('.feedback-form textarea'),
+};
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const formData = {};
+restoreInput();
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log({ email: refs.email.value, message: refs.message.value });
+  e.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function restoreInput() {
+  const savedObject = localStorage.getItem(LOCALSTORAGE_KEY);
+  const parsedObject = JSON.parse(savedObject);
+
+  if (parsedObject) {
+    refs.message.value = parsedObject.message;
+  }
+  if (parsedObject) {
+    refs.email.value = parsedObject.email;
+  }
+}
